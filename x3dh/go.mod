b5f4eab03ee0074b2051fda1c486302f1b18dd20
bynajmniejIgnor <igor.szczepaniak.02@gmail.com>
2025-04-16 19:28:20 +0200

    x3dh

---
+++ x3dh/go.mod
@@ -0,0 +1,5 @@
+module x3dh
+
+go 1.23.7
+
+require golang.org/x/crypto v0.37.0 // indirect
---
+++ x3dh/main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"golang.org/x/crypto/hkdf"
+	"io"
+	"math/big"
+)
+
+var (
+	p = big.NewInt(37)
+	g = big.NewInt(2)
+)
+
+func intToBytes(x *big.Int) []byte {
+	b := x.Bytes()
+	if len(b) < 32 {
+		padded := make([]byte, 32)
+		copy(padded[32-len(b):], b)
+		return padded
+	}
+	return b
+}
+
+func HKDF(dhValues ...*big.Int) []byte {
+	var combined []byte
+	for _, dh := range dhValues {
+		combined = append(combined, intToBytes(dh)...)
+	}
+
+	hash := sha256.New
+	salt := []byte("X3DH-salt")
+	info := []byte("X3DH-info")
+	hkdf := hkdf.New(hash, combined, salt, info)
+
+	sharedSecret := make([]byte, 32)
+	_, err := io.ReadFull(hkdf, sharedSecret)
+	if err != nil {
+		panic(err)
+	}
+
+	return sharedSecret
+}
+
+type Server struct {
+	IK  *big.Int
+	SPK *big.Int
+	OPK *big.Int
+}
+
+type Receiver struct {
+	ik  *big.Int // Private Identity Key
+	spk *big.Int // Private Signed Pre-Key
+	opk *big.Int // Private One-time Pre-Key
+	IK  *big.Int // Public Identity Key
+	SPK *big.Int // Public Signed Pre-Key
+	OPK *big.Int // One-time Pre-Key
+
+	DH []*big.Int
+}
+
+type Sender struct {
+	ik *big.Int // Private Identity Key
+	ek *big.Int // Private Ephemeral Key
+	IK *big.Int // Identity Key
+	EK *big.Int // Ephemeral Key
+
+	DH []*big.Int
+}
+
+func (rec *Receiver) initReceiver(ik, spk, opk int64) {
+	rec.ik = big.NewInt(ik)
+	rec.spk = big.NewInt(spk)
+	rec.opk = big.NewInt(opk)
+
+	rec.IK = new(big.Int).Exp(g, rec.ik, p)
+	rec.SPK = new(big.Int).Exp(g, rec.spk, p)
+	rec.OPK = new(big.Int).Exp(g, rec.opk, p)
+
+	rec.DH = make([]*big.Int, 4)
+}
+
+func (send *Sender) initSender(ik, ek int64) {
+	send.ik = big.NewInt(ik)
+	send.ek = big.NewInt(ek)
+
+	send.IK = new(big.Int).Exp(g, send.ik, p)
+	send.EK = new(big.Int).Exp(g, send.ek, p)
+
+	send.DH = make([]*big.Int, 4)
+}
+
+func (send *Sender) computeDH(server *Server) {
+	send.DH[0] = new(big.Int).Exp(server.SPK, send.ik, p)
+	send.DH[1] = new(big.Int).Exp(server.IK, send.ek, p)
+	send.DH[2] = new(big.Int).Exp(server.SPK, send.ek, p)
+	send.DH[3] = new(big.Int).Exp(server.OPK, send.ek, p)
+}
+
+func (rec *Receiver) computeDH(sender *Sender) {
+	rec.DH[0] = new(big.Int).Exp(sender.IK, rec.spk, p)
+	rec.DH[1] = new(big.Int).Exp(sender.EK, rec.ik, p)
+	rec.DH[2] = new(big.Int).Exp(sender.EK, rec.spk, p)
+	rec.DH[3] = new(big.Int).Exp(sender.EK, rec.opk, p)
+}
+
+func main() {
+	bob := &Receiver{}
+	bob.initReceiver(5, 7, 11)
+
+	alice := &Sender{}
+	alice.initSender(3, 13)
+
+	server := &Server{
+		IK:  bob.IK,
+		SPK: bob.SPK,
+		OPK: bob.OPK,
+	}
+
+	fmt.Println("\nPublic Values:")
+	fmt.Println("Alice's Ephemeral Public Key:", alice.EK)
+	fmt.Println("Alice's Identity Public Key:  ", alice.IK)
+	fmt.Println("Bob's Identity Public Key:    ", bob.IK)
+	fmt.Println("Bob's Signed Pre-Key:        ", bob.SPK)
+	fmt.Println("Bob's One-Time Pre-Key:      ", bob.OPK)
+
+	alice.computeDH(server)
+	fmt.Println("\nAlice DH Results:")
+	fmt.Println("DH1 (IK_A, SPK_B):", alice.DH[0])
+	fmt.Println("DH2 (EK_A, IK_B): ", alice.DH[1])
+	fmt.Println("DH3 (EK_A, SPK_B):", alice.DH[2])
+	fmt.Println("DH4 (EK_A, OPK_B):", alice.DH[3])
+
+	aliceSecret := HKDF(alice.DH...)
+	fmt.Println("\nAlice Secret:", aliceSecret)
+
+	bob.computeDH(alice)
+	fmt.Println("\nBob's DH Results:")
+	fmt.Println("DH1 (SPK_B, IK_A):", bob.DH[0])
+	fmt.Println("DH2 (IK_B, EK_A): ", bob.DH[1])
+	fmt.Println("DH3 (SPK_B, EK_A):", bob.DH[2])
+	fmt.Println("DH4 (OPK_B, EK_A):", bob.DH[3])
+
+	bobSecret := HKDF(bob.DH...)
+	fmt.Println("\nBob Secret:", bobSecret)
+}
